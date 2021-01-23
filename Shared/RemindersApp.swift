4012f68c81dbc99d0dd5d8fc5d2f3c62a4d5b2df
Scott Doggett <sdoggett@lcs.on.ca>
2021-01-23 16:18:08 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Doggett, Scott on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
